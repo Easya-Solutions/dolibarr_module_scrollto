40ba0b14db07e3cc984a344136790a43daaba3c3
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2019-08-09 18:59:18 +0200

    FIX: conflict: some other scroll-setting behaviour may run after scrollTo, causing it to have no effect. This fix wraps scrollTo in a setTimeout(…, 0) to put it at the end of the execution queue

--- js/scrollto.js
+++ js/scrollto.js
@@ -1,31 +1,26 @@
 if($) {
 	$(document).ready(function() {
-		
-		if($.cookie) {
-			var page_y = $.cookie("scrollTo_page_y");
-			var page_url = $.cookie("scrollTo_url");
-			//console.log(page_y,page_url);
-			
-			if(page_y>0 && document.location.href.indexOf(page_url)>-1 ) {
-				$(document).scrollTop(page_y);	
-			}	
-			
-			
-		}
-		
+		setTimeout(
+			function() {
+				if($.cookie) {
+					var page_y = $.cookie("scrollTo_page_y");
+					var page_url = $.cookie("scrollTo_url");
+					//console.log(page_y,page_url);
+
+					if(page_y>0 && document.location.href.indexOf(page_url)>-1 ) {
+						$(document).scrollTop(page_y);
+					}
+				}
+			},
+			0
+		);
 	});
-	
+
 	$(window).on('unload', function() {
-		
 		var page_y = $(document).scrollTop();
-		//console.log(page_y);
 		if($.cookie) {
-		
 			$.cookie("scrollTo_page_y", page_y, { expires: 1, path: '/' });	
 			$.cookie("scrollTo_url", document.location.href.split('?')[0], { expires: 1, path: '/' });
-			
-		}	
+		}
 	});
-	
 }
-
